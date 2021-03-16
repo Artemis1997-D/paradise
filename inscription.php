3d5a0b7f462da540ae7a3be6ebe7ec4a4540178f
Diane Sales <diane.sales@hetic.net>
2021-03-16 23:54:28 +0100

    html and ccs changes in form

--- inscription.php
+++ inscription.php
@@ -4,34 +4,50 @@
 <form class="connexion-inscription">
 <h2 class="text-center mt-5 mb-5">Inscription</h2>
 <div>
+  <section id="name">
 <div class="form-group" >
     <label for="prenom">Prénom</label>
-    <input type="text" class="form-control" id="prenom" placeholder="Entrez votre prénom">
+    <input type="text" class="form-control" id="prenom" placeholder="Votre prénom">
   </div>
   <div class="form-group">
     <label for="nom">Nom</label>
-    <input type="text" class="form-control" id="nom" placeholder="Entrez votre nom">
+    <input type="text" class="form-control" id="nom" placeholder="Votre nom">
   </div>
+  </section>
   </div>
-  <div class="dropdown">
-</div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Civilité</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Femme</option>
+      <option>Homme</option>
+      <option>Autre</option>
+    </select>
+  </div>
   <div class="form-group">
     <label for="mdp">Mot de passe</label>
     <input type="password" class="form-control" id="mdp" placeholder="Mot de passe">
   </div>
   <div class="form-group">
+    <label for="mdp">Confirmation du mot de passe</label>
+    <input type="password" class="form-control" id="mdp-confirm" placeholder="Mot de passe">
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="Email">
+  </div>
+  <div class="form-group">
     <label for="adresse">Adresse Postale</label>
-    <input type="text" class="form-control" id="adresse" placeholder="Entrez votre adresse">
+    <input type="text" class="form-control" id="adresse" placeholder="Adresse postale">
   </div>
   <div class="form-group">
     <label for="telephone">Télephone</label>
-    <input type="tel" class="form-control" id="telephone" placeholder="Entrez votre numéro de téléphone">
+    <input type="tel" class="form-control" id="telephone" placeholder="Votre numéro de téléphone">
   </div>
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">S'inscrire à notre newsletter</label>
   </div>
-  <button type="submit" class="btn btn-primary">S'inscrire</button>
+  <button type="submit" class="btn btn-primary" id="btn-inscrire">S'inscrire</button>
 </form>
 </section>
 
--- login.php
+++ login.php
@@ -11,7 +11,7 @@
     <label for="mdp">Mot de passe</label>
     <input type="password" class="form-control" id="mdp" placeholder="Entrez votre mot de passe">
   </div>
-  <button type="submit" class="btn btn-primary">Se connecter</button>
+  <button type="submit" class="btn btn-primary" id="btn-login">Se connecter</button>
   <p><a href="#">Mot de passe oublié</a>
   <p> Nouveau chez Paradise ? <a href="inscription.php">Inscrivez vous</a>
 </form>
