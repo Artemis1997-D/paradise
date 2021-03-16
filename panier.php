c7d7cfbd7f0be006df51fa6fe3a6d572287e189e
ash-sb <ashley.saib@hetic.net>
2021-03-16 23:20:10 +0100

    CSS panier

--- panier.php
+++ panier.php
@@ -3,12 +3,13 @@
 <h2 class="text-center mt-5 mb-5">Mes commandes</h2>
 <div class="panier">
   <div class="article">
-    <img src="#" alt="#" srcset="#">
+    <img class="image-panier" src="asset\img_produit\isla-paloma-img1.jpg" alt="isla-paloma" >
     <p class="detail">Isla Paloma<br>Panama<br>295 812€</p>
     <a href="#">Supprimer</a>
   </div>
+  <hr>
   <div class="article">
-    <img src="#" alt="#" srcset="#">
+    <img class="image-panier" src="asset\img_produit\sand-point-road-beach.jpg" alt="bungalow" >
     <p class="detail">Isla Paloma<br>Panama<br>295 812€</p>
     <a href="#">Supprimer</a>
   </div>
@@ -16,7 +17,7 @@
 <hr>
 <div class="total">
   <p>Total</p>
-  <p>3 967 989</p>
+  <p>3 967 989€</p>
 </div>
 
 
